d41e259c18221ef3a40606c582125eea9ffa9491
hy4dev <hafifi.yahya@outlook.com>
2025-08-23 17:24:55 +0800

    Try on MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import 'dotenv/config';
+import { db } from './libs/dbConnect.js';
 
 const app = express();
 const PORT = 8000;
 
-app.use('', (req, res) => {
-    res.status(404).json({message: 'not found'});
+// Create a new '/' route.
+app.use('/', (req, res) => {
+    res.status(200).json({message: 'Hello World!'});
 });
 
-// Start the Express server
+// Older version of Express using '*'.
+app.use('', (req, res) => {
+    res.status(404).json({message: 'Not Found'});
+});
+
+// Start the Express server.
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
